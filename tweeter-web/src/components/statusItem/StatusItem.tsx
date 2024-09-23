01d788300ea090b06c663405ef8cacd6509c59ca
jason <whdgur826333@gmail.com>
2024-09-23 16:03:35 -0600

    added userNavigationHook

--- tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -3,46 +3,14 @@ import { AuthToken, FakeData, Status, User } from "tweeter-shared";
 import Post from "./Post";
 import useToastListener from "../toaster/ToastListenerHook";
 import useInfoHook from "../userInfo/UserInfoHook";
+import userNavigationHook from "../userInfo/UserNavigationHook";
 
 interface Props {
   value: Status;
 }
 const StatusItem = (props: Props) => {
-  const { displayErrorMessage } = useToastListener();
-  const { displayedUser, setDisplayedUser, currentUser, authToken } =
-    useInfoHook();
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
+  const { navigateToUser } = userNavigationHook();
 
-    try {
-      const alias = extractAlias(event.target.toString());
-
-      const user = await getUser(authToken!, alias);
-
-      if (!!user) {
-        if (currentUser!.equals(user)) {
-          setDisplayedUser(currentUser!);
-        } else {
-          setDisplayedUser(user);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
   return (
     <div className="col bg-light mx-0 px-0">
       <div className="container px-0">
