490a8348109d0eb37e0d04e312418c8cee34a8a5
jason <whdgur826333@gmail.com>
2024-10-18 22:29:38 -0600

    finished test for logoutpresenter

--- tweeter-web/test/Presenter/LogoutPresenter.test.ts
+++ tweeter-web/test/Presenter/LogoutPresenter.test.ts
@@ -3,7 +3,15 @@ import {
   LogoutPresenter,
   LogoutView,
 } from "../../src/presenters/LogoutPresenter";
-import { instance, mock, spy, verify, when } from "ts-mockito";
+import {
+  anything,
+  capture,
+  instance,
+  mock,
+  spy,
+  verify,
+  when,
+} from "ts-mockito";
 import { UserService } from "../../src/model/service/UserService";
 describe("LogoutPresenter test", () => {
   let mockLogoutPresenterView: LogoutView;
@@ -34,5 +42,26 @@ describe("LogoutPresenter test", () => {
   it("calls logout on the user service with the correct auth token", async () => {
     await logoutPresenter.logOut(authToken);
     verify(mockUserService.logout(authToken)).once();
+
+    // let [capturedAuthToken] = capture(mockUserService.logout).last();
+    // expect(capturedAuthToken).toEqual(authToken);
+  });
+  it("tells the view to clear the last info message and clear the user info.", async () => {
+    await logoutPresenter.logOut(authToken);
+    verify(mockLogoutPresenterView.clearLastInfoMessage()).once();
+    verify(mockLogoutPresenterView.clearUserInfo()).once();
+  });
+  it("tells the view to display an error message and does not tell it to clear the last info message or clear the user info", async () => {
+    const error = new Error("An error occured");
+    when(mockUserService.logout(authToken)).thenThrow(error);
+    await logoutPresenter.logOut(authToken);
+    verify(
+      mockLogoutPresenterView.displayErrorMessage(
+        `Failed to log user out because of exception: An error occured`
+      )
+    ).once();
+
+    verify(mockLogoutPresenterView.clearLastInfoMessage()).never();
+    verify(mockLogoutPresenterView.clearUserInfo()).never();
   });
 });
