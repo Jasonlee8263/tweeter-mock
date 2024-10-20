8f3b25f943aa800bcac5037cf99942cdb1015e04
jason <whdgur826333@gmail.com>
2024-10-19 21:04:38 -0600

    finished testing post status service

--- tweeter-web/test/Presenter/PostPresenter.test.ts
+++ tweeter-web/test/Presenter/PostPresenter.test.ts
@@ -1,7 +1,15 @@
-import { AuthToken, User } from "tweeter-shared";
+import { AuthToken, Status, User } from "tweeter-shared";
 import { StatusService } from "../../src/model/service/StatusService";
 import { PostPresenter, PostView } from "../../src/presenters/PostPresenter";
-import { instance, mock, spy, verify, when } from "ts-mockito";
+import {
+  capture,
+  deepEqual,
+  instance,
+  mock,
+  spy,
+  verify,
+  when,
+} from "ts-mockito";
 describe("PostPresenter test", () => {
   let mockPostPresenterView: PostView;
   let postPresenter: PostPresenter;
@@ -9,6 +17,7 @@ describe("PostPresenter test", () => {
   const post = "This is a post";
   const user = new User("jason", "lee", "j", "test");
   const authToken = new AuthToken("abc123", Date.now());
+  const status = new Status(post, user, Date.now());
 
   beforeEach(() => {
     mockPostPresenterView = mock<PostView>();
@@ -26,6 +35,21 @@ describe("PostPresenter test", () => {
     await postPresenter.submitPost(post, user, authToken);
     verify(
       mockPostPresenterView.displayInfoMessage("Posting status...", 0)
+    ).once();
+  });
+
+  it("calls postStatus on the post status service with the correct status string and auth token.", async () => {
+    await postPresenter.submitPost(post, user, authToken);
+    let [capturedAuthToken, capturedPostStatus] = capture(
+      mockStatusService.postStatus
+    ).last();
+    console.log(capturedPostStatus);
+    const expectedStatus = new Status(post, user, capturedPostStatus.timestamp);
+    verify(
+      mockStatusService.postStatus(
+        deepEqual(authToken),
+        deepEqual(expectedStatus)
+      )
     ).once();
   });
 });
