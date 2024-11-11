8cb063a613897b736cf88d2dbaed538b8ab5d5d3
jason <whdgur826333@gmail.com>
2024-11-10 22:18:17 -0700

    feat: added GetFeedLambda and GetStoryLambda

--- tweeter-server/src/model/service/StatusService.ts
+++ tweeter-server/src/model/service/StatusService.ts
@@ -1,31 +1,39 @@
-import { AuthToken, FakeData, Status } from "tweeter-shared";
+import { AuthToken, FakeData, Status, StatusDto } from "tweeter-shared";
 export class StatusService {
-    public async loadMoreFeedItems(
-        authToken: AuthToken,
-        userAlias: string,
-        pageSize: number,
-        lastItem: Status | null
-      ): Promise<[Status[], boolean]> {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-      };
-    public async loadMoreStoryItems(
-    authToken: AuthToken,
+  public async loadMoreFeedItems(
+    token: string,
     userAlias: string,
     pageSize: number,
-    lastItem: Status | null
-    ): Promise<[Status[], boolean]>{
+    lastItem: StatusDto | null
+  ): Promise<[StatusDto[], boolean]> {
     // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-    };
+    return this.getFakeData(lastItem, pageSize);
+  }
+  public async loadMoreStoryItems(
+    token: string,
+    userAlias: string,
+    pageSize: number,
+    lastItem: StatusDto | null
+  ): Promise<[StatusDto[], boolean]> {
+    // TODO: Replace with the result of calling server
+    return this.getFakeData(lastItem, pageSize);
+  }
+  private async getFakeData(
+    lastItem: StatusDto | null,
+    pageSize: number
+  ): Promise<[StatusDto[], boolean]> {
+    const [items, hasMore] = FakeData.instance.getPageOfStatuses(
+      Status.fromDto(lastItem),
+      pageSize
+    );
+    const dtos = items.map((status) => status.dto);
+    return [dtos, hasMore];
+  }
 
-    public async postStatus(
-      authToken: AuthToken,
-      newStatus: Status
-    ): Promise<void> {
-      // Pause so we can see the logging out message. Remove when connected to the server
-      await new Promise((f) => setTimeout(f, 2000));
-  
-      // TODO: Call the server to post the status
-    };
+  public async postStatus(token: string, newStatus: Status): Promise<void> {
+    // Pause so we can see the logging out message. Remove when connected to the server
+    await new Promise((f) => setTimeout(f, 2000));
+
+    // TODO: Call the server to post the status
+  }
 }
--- tweeter-shared/src/index.ts
+++ tweeter-shared/src/index.ts
@@ -10,14 +10,19 @@ export { AuthToken } from "./model/domain/AuthToken";
 
 //DTOs
 export type { UserDto } from "./model/dto/UserDto";
+export type { StatusDto } from "./model/dto/StatusDto";
+export type { PostSegmentDto } from "./model/dto/PostSegmentDto";
 
 //Requests
 export type { PagedUserItemRequest } from "./model/net/request/PagedUserItemRequest";
 export type { GetFollowCountRequest } from "./model/net/request/GetFollowCountRequest";
 export type { GetIsFollowerStatusRequest } from "./model/net/request/GetIsFollowerStatusRequest";
+export type { PagedStatusItemRequest } from "./model/net/request/PagedStatusItemRequest";
+
 //Response
 export type { PagedUserItemResponse } from "./model/net/response/PagedUserItemResponse";
 export type { GetFollowCountResponse } from "./model/net/response/GetFollowCountResponse";
 export type { GetIsFollowerStatusResponse } from "./model/net/response/GetIsFollowerStatusResponse";
+export type { PagedStatusItemResponse } from "./model/net/response/PagedStatusItemResponse";
 
 export { FakeData } from "./util/FakeData";
